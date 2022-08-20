66711c01594ce9f62a325a055449607c203387a8
kanhasriv <54784441+kanhasriv@users.noreply.github.com>
2022-08-20 13:20:07 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,11 +27,11 @@ app.use("/api/v1",userRoute)
 app.use("/api/v1",orderRoute)
 app.use("/api/v1",paymentRoute)
 
-app.use(express.static(path.join(__dirname,"../frontend/build")))
+// app.use(express.static(path.join(__dirname,"../frontend/build")))
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
-})
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+// })
 
 //Middleware for Errors
 app.use(errorMiddleware)
